b60658f87093c136911744fdf4308c3810887b85
Taha-Alawa <Tahaalawa366@gamail.com>
2023-08-28 10:52:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import Navbar from "/src/components/Navbar"
+import Hero from "/src/components/Hero"
+import Card from "/src/components/Card"
+import data from "/src/data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
